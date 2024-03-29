d41afe02b8c7d33063ffa6122231d3e60f7cfaf1
Durgaprasad-Budhwani <durgaprasad.budhwani@gmail.com>
2024-03-29 10:34:21 +0530

    Refactor test script and improve translator functionality
    In this commit, the test script was removed and significant changes were made to the text translator functionality, notably by simplifying and reorganizing the translation rules and moving them into the main 'llm' file, enhancing code maintainability. New dependencies were added to the 'package.json' file, likely to support these changes to translation functionality.

--- src/app/bedrock.tsx
+++ src/app/bedrock.tsx
@@ -7,17 +7,17 @@ import sharedIniFileLoader from "@aws-sdk/shared-ini-file-loader";
 import TextInput from 'ink-text-input';
 import { Config, SelectItem } from "./type.js";
 
-const profileOrAwsKey = [
-	{label: 'Profile', value: 'profile'},
-	{label: 'AWS Key', value: 'aws-key'}
-];
+// const profileOrAwsKey = [
+// 	{label: 'Profile', value: 'profile'},
+// 	{label: 'AWS Key', value: 'aws-key'}
+// ];
 
 
 type Props = {
 	onComplete: (config: Config) => void;
 }
 const Bedrock = ({onComplete}: Props) => {
-	const [userProfileOrKeys, setUserProfileOrKeys] = useState<string>("");
+	const [userProfileOrKeys, setUserProfileOrKeys] = useState<string>("aws-key");
 	const [accessKey, setAccessKey] = useState<string>("");
 	const [secretKey, setSecretKey] = useState<string>("");
 	const [region, setRegion] = useState<string>("");
@@ -55,6 +55,7 @@ const Bedrock = ({onComplete}: Props) => {
 		setLoading(false);
 	}, []);
 
+	// @ts-ignore
 	const handleAccessMethodSelection = async (item: SelectItem) => {
 		setUserProfileOrKeys(item.value);
 		if (item.value === 'profile') {
@@ -73,26 +74,26 @@ const Bedrock = ({onComplete}: Props) => {
 		);
 	}
 
-	if (!userProfileOrKeys) {
-		return (
-			<Box width="100%" flexDirection="column">
-				<Box width="100%" paddingBottom={1} flexDirection="column">
-					<Link url="https://aws.amazon.com/bedrock/?">
-						<Text color="cyan">Bedrock</Text>
-					</Link>
-				</Box>
-				<Box flexDirection="column">
-					<Box>
-						<Text>How you want to access the models?</Text>
-					</Box>
-					<SelectInput
-						items={profileOrAwsKey}
-						onSelect={handleAccessMethodSelection}
-					/>
-				</Box>
-			</Box>
-		);
-	}
+	// if (!userProfileOrKeys) {
+	// 	return (
+	// 		<Box width="100%" flexDirection="column">
+	// 			<Box width="100%" paddingBottom={1} flexDirection="column">
+	// 				<Link url="https://aws.amazon.com/bedrock/?">
+	// 					<Text color="cyan">Bedrock</Text>
+	// 				</Link>
+	// 			</Box>
+	// 			<Box flexDirection="column">
+	// 				<Box>
+	// 					<Text>How you want to access the models?</Text>
+	// 				</Box>
+	// 				<SelectInput
+	// 					items={profileOrAwsKey}
+	// 					onSelect={handleAccessMethodSelection}
+	// 				/>
+	// 			</Box>
+	// 		</Box>
+	// 	);
+	// }
 
 	if (userProfileOrKeys === 'profile') {
 		if (!profileKey) {
--- test.ts
+++
@@ -1,4 +0,0 @@
-import processor from "./src/processor";
-
-processor("/Volumes/Code/DCoderAI/site/posts/balancing-security-with-user-convenience-in-new-technologies.mdx", "/Volumes/Code/DCoderAI/site/posts/balancing-security-with-user-convenience-in-new-technologies.de.mdx", "German")
-
